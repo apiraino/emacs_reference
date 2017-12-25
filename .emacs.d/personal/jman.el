f3d8a9798e79c1c2b57f2c84659d8c10fcf7cda8
apiraino <apiraino@users.noreply.github.com>
2017-12-25 18:51:39 +0100

    removed some warnings, added (and disabled) nlinum mode

--- .emacs.d/personal/jman.el
+++ .emacs.d/personal/jman.el
@@ -1,9 +1,9 @@
 ;; Set repo, download use-package package (wtf)
 ; list the packages you want
-(setq package-list '(use-package ag flycheck elpy flycheck-rust racer yaml-mode py-autopep8 ))
+(defvar package-list '(use-package ag flycheck elpy flycheck-rust racer yaml-mode py-autopep8))
 ; Repos
-(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")))
+(defvar package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                           ("melpa" . "https://melpa.org/packages/")))
 ; activate all the packages (in particular autoloads)
 (package-initialize)
 ; fetch the list of packages available
@@ -14,9 +14,18 @@
   (unless (package-installed-p package)
     (package-install package)))
 
+; nlinum broken as of v1.8.1
+; Activate and configure nlinum
+;(global-linum-mode t)
+;(use-package nlinum
+;  :init
+;  (defvar nlinum-format "%d ")
+;  (defvar nlinum-highlight-current-line t)
+;  )
+
+
 ;; Get and config Jedi (Python IDE)
 (use-package jedi
-  :ensure t
   :init
   (add-hook 'python-mode-hook 'jedi:setup)
   (setq jedi:environment-root ".")
@@ -25,8 +34,8 @@
 
 ;; Python header on new buffers
 (auto-insert-mode)  ;;; Adds hook to find-files-hook
-(setq auto-insert-directory "~/.emacs.d/personal/") ;;; Or use custom, *NOTE* Trailing slash important
-(setq auto-insert-query nil) ;;; If you don't want to be prompted before insertion
+(defvar auto-insert-directory "~/.emacs.d/personal/") ;;; Or use custom, *NOTE* Trailing slash important
+(defvar auto-insert-query nil) ;;; If you don't want to be prompted before insertion
 (define-auto-insert "\.py" "my-python-template.py")
 
 ;; Enable AbbrevMode
@@ -35,16 +44,16 @@
       "~/.emacs.d/personal/abbrev_defs")
 (setq-default abbrev-mode t)
 
-(setq default-tab-width 4)
+(setq tab-width 4)
 
 ; disable stupid spell checker
-(setq prelude-flyspell nil)
+(defvar prelude-flyspell nil)
 
 
 ; Python linter (pep8) on save
 
 ; Only run flymake if I've not been typing for 5 seconds
-(setq flymake-no-changes-timeout 5)
+(defvar flymake-no-changes-timeout 5)
 (use-package py-autopep8
   :init
   (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save))
@@ -53,7 +62,11 @@
 ; Rust stuff
 
 ; run rustfmt everytime you save a buffer
-(setq rust-format-on-save t)
+(use-package rust-mode
+  :mode "\\.rs\\'"
+  :init
+  (defvar rust-format-on-save t))
+
 
 ;; flycheck-rust
 (use-package flycheck-rust
