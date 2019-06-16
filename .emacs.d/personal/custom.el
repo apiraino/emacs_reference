2b7ce9f0732e9fa97f473ef607443b3a79dd0fc0
apiraino <apiraino@users.noreply.github.com>
2019-06-16 19:26:10 +0200

    TESTING: do not set any custom variables for Rust modules

--- .emacs.d/personal/custom.el
+++ .emacs.d/personal/custom.el
@@ -4,18 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(clean-buffer-list-delay-general 0)
- '(flycheck-disabled-checkers (quote (rust)))
- '(flycheck-rust-check-tests nil)
  '(org-export-backends (quote (html md odt)))
  '(org-journal-date-format "%Y-%m-%d")
  '(package-selected-packages
    (quote
-    (yasnippet-snippets yasnippet scss-mode less-css-mode dockerfile-mode rcirc-notify ox-mediawiki ox-wk xclip org-journal csv-mode org-ac markdown-mode jedi git-gutter py-autopep8 yaml-mode racer flycheck-rust use-package zop-to-char which-key volatile-highlights undo-tree smartrep smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window)))
- '(safe-local-variable-values
-   (quote
-    ((flycheck-disabled-checkers emacs-lisp-checkdoc)
-     (flycheck-flake8rc . "setup.cfg")
-     (column-number-mode . 120)))))
+    (xclip org-journal git-gutter py-autopep8 racer ox-wk jedi use-package yaml-mode geiser flycheck-rust cargo rust-mode company-anaconda anaconda-mode lsp-ui company-lsp json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number move-text magit projectile imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring beacon anzu ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
