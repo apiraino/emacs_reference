8134aa5de6da7220ee9266e9acaa9da99e07ca84
apiraino <apiraino@users.noreply.github.com>
2020-01-18 21:24:15 +0100

    Update EMACS conf (for wayland, new ox-odt, ...)

--- .emacs.d/personal/custom.el
+++ .emacs.d/personal/custom.el
@@ -6,9 +6,18 @@
  '(clean-buffer-list-delay-general 0)
  '(org-export-backends (quote (html md odt)))
  '(org-journal-date-format "%Y-%m-%d")
+ '(package-archives
+   (quote
+    (("gnu" . "https://elpa.gnu.org/packages/")
+     ("melpa" . "https://melpa.org/packages/")
+     ("ox-odt" . "https://kjambunathan.github.io/elpa/"))))
  '(package-selected-packages
    (quote
-    (xclip org-journal git-gutter py-autopep8 racer ox-wk jedi use-package yaml-mode geiser flycheck-rust cargo rust-mode company-anaconda anaconda-mode lsp-ui company-lsp json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number move-text magit projectile imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring beacon anzu ace-window))))
+    (ox-odt paredit refine dockerfile-mode cmake-mode scss-mode web-mode mmm-mode php-mode flycheck-flow rg xclip org-journal git-gutter py-autopep8 racer ox-wk jedi use-package yaml-mode geiser flycheck-rust cargo rust-mode company-anaconda anaconda-mode lsp-ui company-lsp json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights super-save smartrep smartparens operate-on-number move-text magit projectile imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring beacon anzu ace-window)))
+ '(xclip-method (quote wl-copy))
+ '(xclip-mode t)
+ '(xclip-program "wl-copy")
+ '(xclip-select-enable-clipboard t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
